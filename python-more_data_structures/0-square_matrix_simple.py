e8ea332a696848739d4334f81e6c06b9f75c3985
ibadvahidov505 <ibadvahidov505@gmail.com>
2026-03-28 23:43:16 +0400

    elmin

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,16 @@
+def square_matrix_simple(matrix=[]):
+    newone=[]
+    for row in matrix:
+        newrow=[]
+        for i in row:
+            j=i**2
+            newrow.append(j)
+        newone.append(newrow)
+        print('\n')
+    return(newone)
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(square_matrix_simple(matrix))
