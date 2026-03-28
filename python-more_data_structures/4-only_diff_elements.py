c474eebbf1c03ca2b151a185bfac525eb75f56ef
ibadvahidov505 <ibadvahidov505@gmail.com>
2026-03-29 00:35:20 +0400

    elmin

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -4,4 +4,7 @@ def only_diff_elements(set_1, set_2):
     for i in set_1:
         if i not in set_2:
             difr.append(i)
+    for k in set_2:
+        if k not in set_1:
+            difr.append(k)
     return difr
