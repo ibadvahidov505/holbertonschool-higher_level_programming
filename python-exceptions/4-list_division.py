f6f015218ed2312909565150b23084078da7dc88
ibadvahidov505 <ibadvahidov505@gmail.com>
2026-03-29 03:11:03 +0400

    elmin

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    newone = []
+    """Divides element by element 2 lists."""
+    new_list = []
     for i in range(list_length):
-        res = 0  # Hər addımda nəticəni əvvəlcə 0 təyin edirik
+        res = 0
         try:
-            # Bölməni yerinə yetirməyə çalışırıq
             res = my_list_1[i] / my_list_2[i]
-        except TypeError:
+        except (ValueError, TypeError):
             print("wrong type")
         except ZeroDivisionError:
             print("division by 0")
         except IndexError:
             print("out of range")
         finally:
-            # Xəta olsa da, olmasa da nəticəni (ya bölməni, ya 0-ı) əlavə edirik
-            newone.append(res)
-    return newone
+            new_list.append(res)
+    return new_list
