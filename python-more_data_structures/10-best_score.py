cf5a0ce78b4c8a975a1bb009ffc373faec216213
ibadvahidov505 <ibadvahidov505@gmail.com>
2026-03-29 01:29:30 +0400

    elmin

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    """
+    Returns a key with the biggest integer value.
+    """
     if not a_dictionary:
-        return None 
+        return None
+
+    # max(dictionary) normalda açarları (key) əlifba sırası ilə müqayisə edir.
+    # Amma key=a_dictionary.get əlavə etdikdə, o, açarları DEYERLERE görə müqayisə edir.
     return max(a_dictionary, key=a_dictionary.get)
