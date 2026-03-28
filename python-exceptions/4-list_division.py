2ba57221c1e1be19248cb56386045e4615af7655
ibadvahidov505 <ibadvahidov505@gmail.com>
2026-03-29 03:12:13 +0400

    elmin

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,7 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
         res = 0
         try:
             res = my_list_1[i] / my_list_2[i]
-        except (ValueError, TypeError):
+        except (TypeError, ValueError):
             print("wrong type")
         except ZeroDivisionError:
             print("division by 0")
