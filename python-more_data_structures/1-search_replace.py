a8397aa27099a8e296262d543e91b7a636fe0745
ibadvahidov505 <ibadvahidov505@gmail.com>
2026-03-29 00:14:51 +0400

    elmin

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,10 +2,8 @@
 def search_replace(my_list, search, replace):
     new_list=[]
     for i in my_list:
-        if i==2:
-            i=89
-            new_list.append(i)
+        if i==search:
+            new_list.append(replace)
         else:
             new_list.append(i)
-    return (new_list)
-    return (my_list)
+    return (new_list))
