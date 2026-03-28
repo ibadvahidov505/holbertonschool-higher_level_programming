cc515ffcf36b24f07d45e2ec758cfdb1fee1dd3e
ibadvahidov505 <ibadvahidov505@gmail.com>
2026-03-29 02:50:47 +0400

    elmin

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    """Prints the first x elements of a list and only integers."""
     count = 0
     for i in range(x):
         try:
-            # Siyahıdan elementi götürürük və integer formatına salırıq
-            # Əgər my_list[i] yoxdursa, burada IndexError yaranacaq və
-            # funksiya (və proqram) məhz burada dayanacaq.
             print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
-            # Yalnız formatlama xətası olanda keçirik (skip)
-            pass
+            continue
+    print("")
     return count
