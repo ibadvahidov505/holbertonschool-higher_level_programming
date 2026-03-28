df3e1459137a0815b6b550266483aa14b95739ab
ibadvahidov505 <ibadvahidov505@gmail.com>
2026-03-29 00:28:41 +0400

    elmin

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    s = 0
     newone = []
     for i in my_list:
         if i not in newone:
-          newone.append(i)
-          s = sum(newone)    
-    return s
+            newone.append(i)
+    return sum(newone)
